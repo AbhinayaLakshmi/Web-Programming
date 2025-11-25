c44c786628ee326ca3eec6d3139139a3b5933368
AbhinayaLakshmi <230701004@rajalakshmi.edu.in>
2025-11-26 03:17:17 +0530

    Add files via upload

---
+++ MY_MOBILES_APP-PROJECT/mobilerouter.js
@@ -0,0 +1,21 @@
+//router/mobilerouter.js
+
+const router = require("express").Router();
+const {
+  getAllMobiles,
+  getMobilesByUserId,
+  deleteMobile,
+  updateMobile,
+  getMobileById,
+  addMobile,
+} = require("../controllers/mobileController");
+
+router.post("/", getAllMobiles);
+router.post("/seller", getMobilesByUserId); // matches your frontend /seller endpoint which posts body { userId,... }
+router.delete("/:id", deleteMobile);
+router.put("/:id", updateMobile);
+router.get("/:id", getMobileById);
+router.post("/add", addMobile);
+
+module.exports = router;
+
---
+++ MY_MOBILES_APP-PROJECT/userrouter.js
@@ -0,0 +1,10 @@
+//router/userrouter.js
+
+const router = require("express").Router();
+const { getUserByUsernameAndPassword, addUser, getAllUsers } = require("../controllers/userController");
+
+router.post("/login", getUserByUsernameAndPassword);
+router.post("/", addUser);
+router.get("/", getAllUsers);
+
+module.exports = router;
